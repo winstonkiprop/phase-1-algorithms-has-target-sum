bc1d85487e54d04194b1296c707b39c81c7c99ff
winston kiprop <winstonekip50@gmail.com>
2022-06-20 11:50:36 +0300

    commit tree 0e947c91b8b5d4c8983f844b92742e430475372c

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  for(var i=0; i<array.length; i++){
+    for(var j=i+1; j<array.length; j++){
+      if(array[i] + array[j] === target){
+        return true;
+      }
+
+
+    }
+  }
+  return false;
+
+
+
 }
 
 /* 
